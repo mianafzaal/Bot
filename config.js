a4b36b3da486fb1f7521253f940a632e54c1999b
mian <96155069+mianafzaal@users.noreply.github.com>
2023-07-11 02:17:24 +0500

    Update config.js

--- config.js
+++ config.js
@@ -39,7 +39,7 @@ global.prems = ['972529277314'],
 //  en = English         pt = Português 
 //  ar = عرب  
 
-global.lenguajeGB = es  //Idioma de The-LoliBot-MD, Ejemplo: es | en | pt...
+global.lenguajeGB = en  //Idioma de The-LoliBot-MD, Ejemplo: es | en | pt...
 
 //━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
 
